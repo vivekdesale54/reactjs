f2a8073bc6f7ca70d8386a6fba454eea70f4d8d7
vivek Desale <60285597+vivekdesale54@users.noreply.github.com>
2024-06-02 12:00:00 +0530

    Commit with backdated timestamp

--- src/component/Createtweet.js
+++ src/component/Createtweet.js
@@ -1,11 +1,22 @@
-import React from "react";
+// import React, {useState} from "react";
 
 
-const CreateTweet = () => {
+const CreateTweet = ({tweets, setInput, textInput, setTweets}) => {
+    //Functions
+     const UserInputHandler = (e) => {
+        setInput(e.target.value);
+        // console.log(e.target.value);        
+    };
+    const SubmitHandler = (e) => {
+        e.preventDefault();
+        setTweets([...tweets, textInput]);
+        setTweets('');
+    };
+   
     return(
-        <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'start', marginBottom: '1rem'}}>
-            <textarea cols="50" rows="5" style={{marginBottom: '1rem'}}></textarea>
-            <button style={{cursor: 'pointer'}}>Submit</button>
+        <form onSubmit={SubmitHandler} style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'start', marginBottom: '1rem'}}>
+            <textarea value={textInput} onChange={UserInputHandler} cols="50" rows="5" style={{marginBottom: '1rem'}}></textarea>
+            <button style={{cursor: 'pointer'}}>Click me</button>
         </form>
     );
 };
