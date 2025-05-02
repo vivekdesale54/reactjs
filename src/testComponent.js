63fadc1ecd2ac08acd5ebdc238ebc9a72ecfe08a
vivek Desale <60285597+vivekdesale54@users.noreply.github.com>
2025-05-02 12:00:00 +0530

    test

---
+++ src/testComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TestComponent() {
+    return (
+        <div>
+        <h2>Hello</h2>
+        <h3>From test Component</h3>
+        </div>
+    )
+}
+
+export default TestComponent;
