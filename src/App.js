58062ffb2689cc0215ff42092829e41a3adb6407
vivek Desale <60285597+vivekdesale54@users.noreply.github.com>
2025-05-19 13:28:49 +0530

    learning changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'start', height: '100vh'}}>
+      <h1 style={{color: 'red', font: 'lato'}}>Hello React</h1>
+      <Button />  <br />
+      <br />
+      <Button2/>
+      <p>Welcome to the world of React!</p>
+      <Btn3 />
     </div>
   );
 }
 
+function Button() {
+  return (
+    <button>Submit</button>
+  );
+}
+
+
+function Button2() {
+  return (
+    <button>Manage your data</button>
+  );
+}
+
+function Btn3() {
+  return(
+    <button style={{color: 'red', height: '4rem', width: '8rem'}}>Submit the data you want</button>
+  );
+}
+ 
 export default App;
