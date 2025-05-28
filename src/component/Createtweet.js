5efb064a7a5ef89bc1e7465e909cc715126bf462
vivek Desale <60285597+vivekdesale54@users.noreply.github.com>
2025-05-28 22:53:11 +0530

    add files

--- src/component/Createtweet.js
+++ src/component/Createtweet.js
@@ -1,16 +1,12 @@
 import React from "react";
 
+
 const CreateTweet = () => {
     return(
-        <form
-        onSubmit={(e) => {
-            e.preventDefault();
-        }}
-        style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'start', marginBottom: '1rem'}}>
+        <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'start', marginBottom: '1rem'}}>
             <textarea cols="50" rows="5" style={{marginBottom: '1rem'}}></textarea>
             <button style={{cursor: 'pointer'}}>Submit</button>
         </form>
-    
     );
 };
 
