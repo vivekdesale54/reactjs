fd4c1b614ef0270d945fc027fcc02b82c9aba750
vivek Desale <60285597+vivekdesale54@users.noreply.github.com>
2025-05-29 13:22:50 +0530

    learning changes

--- src/component/tweet.js
+++ src/component/tweet.js
@@ -1,25 +1,15 @@
 import React from "react";
 
 
-const Tweet = ({name, message, name1, name2, message1, message2}) => {
+const Tweet = ({ name, message }) => {
     return (
-        <>
-        <div className="tweet" style={{display: 'flex', flexDirection: 'column' ,alignItems: 'center', justifyContent: 'start', marginBottom: '1.5rem',}}>
+        <div className="tweet" style={{display: 'flex', flexDirection: 'column' ,alignItems: 'center', justifyContent: 'start'}}>
             <h2>{name}</h2>
             <h3>{message}</h3>
-            <h2>{name1}</h2>
-            <h2>{message1}</h2>
-            <h2>{name2}</h2>
-            <h2>{message2}</h2>
             <button style={{cursor: 'pointer', marginBottom: '1.5rem'}}>Delete</button>
             <button style={{cursor: 'pointer'}}>Like</button>
         </div>
-        <div>
-            <p>Hello world</p>
-        </div>
-        </>
     );
 }; 
-
 
 export default Tweet;
