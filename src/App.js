e2a8549def120e3c08a14c2ca87ba5a1c97972a5
vivek Desale <60285597+vivekdesale54@users.noreply.github.com>
2025-06-01 12:52:40 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,51 +1,33 @@
-import React from 'react';
-import Tweet from './component/tweet.js';
+// import React from 'react';
+// import Tweet from './component/tweet.js';
 import CreateTweet from './component/Createtweet.js';
+import Tweet from './component/tweet.js';
+// import {useState} from 'react';
 
 
 function App() {
-  const name = "John Doe";
-  const message = "This is going out of the way";
-  const name2 = "Jane Dow";
-  const newmessage = () => {
-    alert("Hello, world from react!");
-  }
-    return (
+  const name = "Jhonny depp";
+  const message = "I am a pirate";
+  
+  return (
     <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'start', height: '100vh'}}>
-      <h1 style={{color: 'black', font: 'Poppins'}}>Hello React</h1>
-      <h1>Hello {name2}</h1>
       <CreateTweet /> 
+      <Tweet name={name} message={message} />
       <Button2 />
-      <Tweet name={name} message ={message} />
-      <button onClick={newmessage}>Click me</button>
-      <p>Welcome to the world of React!</p>
-      <Btn3 />
-      <Creategreet />
     </div>
   );
 }
 
 function Button2() {
   return (
-    <button style= {{color: 'red', fontsize: '5rem', height: '1.5rem',display: 'flex', alignItems: 'center', justifyContent: 'center'}}>Manage your data</button>
+    <button style= {{color: 'red', fontsize: '5rem', height: '1.5rem',display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: "2rem"}}>Manage your data</button>
 
   );
 }
 
-function Btn3() {
-  return(
-    <button style={{color: 'red', height: '4rem', width: '8rem'}}>Submit the data you want</button>
-  );
-}
 
-function Creategreet() {
-  const greet = "Welcome to learn React";
-  return (
-    <div>
-      <h1 style={{color: "red", fontsize: "10rem", fontFamily: "Plus Jakarta Sans"}}>{greet}</h1>
-    </div>
-  ); 
-}
+
+
 
 
 export default App;
