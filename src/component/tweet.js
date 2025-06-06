cc3ac629a37eb690041eb1dc84574347a7c54965
vivek Desale <60285597+vivekdesale54@users.noreply.github.com>
2025-06-06 21:18:06 +0530

    learning

--- src/component/tweet.js
+++ src/component/tweet.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 
-const Tweet = ({ name, message }) => {
+const Tweet = ({ name, tweets, setName }) => {
+
     return (
         <div className="tweet" style={{display: 'flex', flexDirection: 'column' ,alignItems: 'center', justifyContent: 'start'}}>
             <h2>{name}</h2>
-            <h3>{message}</h3>
+            <h3>{tweets}</h3>
             <button style={{cursor: 'pointer', marginBottom: '1.5rem'}}>Delete</button>
             <button style={{cursor: 'pointer'}}>Like</button>
         </div>
